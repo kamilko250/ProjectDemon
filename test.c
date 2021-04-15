ad0ff957f284bc67cdb593d1d83a6ec831a6257d
Kucznsky <michal.kuczysnki2000@wp.pl>
2021-04-15 03:03:06 +0200

    testy ClearCatalogs i UpdateFile, mozliwe, ze nie dzialaja

--- test.c
+++ test.c
@@ -13,14 +13,17 @@ int main()
 
     //==================================================================
     //printf("test_getLastModificationTime:\n");
-    /*time_t data_modyfikacji;
-    double data_modyfikacji_;
-    data_modyfikacji=getLastModificationTime("/home/michal/Documents/ProjectDemon/demonlib.h");
-    data_modyfikacji_=difftime(data_modyfikacji,0);
-    printf("%g\n",data_modyfikacji_);
-    */
+    //time_t data_modyfikacji;
+    //printf("%lld\n", (long long) data_modyfikacji);
+    //double data_modyfikacji_;
+    //data_modyfikacji=getLastModificationTime("/home/michal/Documents/ProjectDemon/demonlib.h");
+    //data_modyfikacji_=difftime(data_modyfikacji,0);
+    //printf("%g\n",data_modyfikacji_);
+    
    //nie jestem pewien czy dziala poprawnie
    //probowalem wyswietlic czas w bardziej czytelnej formie niz ciag cyfr metoda ze StackOverflow ale póki co bez zalozonego skutku
+   //update: probowalem innych metod na printf zwracanej wartosci w innej formie niz ciag liczb, bez sukcesu
+   //na stacku ktos napisal ze sie nie da, mysle ze to mozliwe :p
    
    //==================================================================
    printf("test_pathToFile:\n");
@@ -35,5 +38,13 @@ int main()
     printf("%s\n",_outpath);
     //przykladowe dzialanie: dla powyzszych parametrow tworzy nowa sciezke "/home/michal/Documents/ProjectDemon/target/a.txt"
     //dziala
+
+    //==================================================================
+    updateFile("/home/michal/Documents/ProjectDemon/source","/home/michal/Documents/ProjectDemon/target",100);
+    //funkcja nie kopiuje plikow znajujacych sie w katalogu source do katalogu target
+
+    //==================================================================
+    clearCatalogs("/home/michal/Documents/ProjectDemon/source","/home/michal/Documents/ProjectDemon/target",1);
+    //niezaleznie od parametru recurSync, funkcja nie zmienia nic w zawartosciach katalogow
     return 0;
 }
